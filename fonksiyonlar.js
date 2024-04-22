1123b350932f8c56b7b6f726aca3da9eff58557f
serdkan <serdar.kandiran@outlook.com>
2024-04-22 20:57:10 +0300

    java calismalar

---
+++ fonksiyonlar.js
@@ -0,0 +1,10 @@
+function isimsoyisim(adiniz, soyadiniz)
+{
+  alert("kullanıcının adı : " + adiniz + " kullanıcının soyadı :" + soyadiniz);
+ 
+}
+
+let adi = prompt("adınız");
+let soyadi = prompt("soyadınız");
+
+isimsoyisim(adi,soyadi);
